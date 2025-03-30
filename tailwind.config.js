a412fc407d685695c17df4b9bf509b17e73ea8d5
Nishant-Kumar-Sah <nishant.sah2001@gmail.com>
2025-03-30 22:59:10 +0530

    remote-code-executor frontend setup

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+import daisyui from "daisyui";
+import typography from "@tailwindcss/typography";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui, typography],
+  daisyui: {
+    themes: ["forest"],
+  },
+};
